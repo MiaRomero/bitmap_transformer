33d9902d984e33060492ff324f7b7123f192517e
MARIA ROMERO <mariaromero18e@gmail.com>
2016-04-11 09:02:55 -0700

    clean up code

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var chosenTransform = process.argv[3] || 'grayscale';
 var color = process.argv[4] || 'red';
 
 fs.readFile(__dirname + '/images/' + image + '.bmp', (error, data) => {
-  if (error) return console.log(error);
+  if (error) return console.dir(error);
   transform.transformImage(data, chosenTransform, color);
 });
